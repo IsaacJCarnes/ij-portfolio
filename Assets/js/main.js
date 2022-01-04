57dabab8fe65905d83fbbc82a38b15208075a002
Isaac Joshua <isaacjcarnes@gmail.com>
2022-01-03 22:03:12 -0500

    Moved displayed work section into a modal

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,9 +1,10 @@
 const showcaseArea = document.getElementById("showcaseArea");
 const highlight = document.getElementById("highlight");
 
-var Project = function (name, link, photo, altTxt) {
+var Project = function (name, deployedLink, projectLink, photo, altTxt) {
   this.name = name;
-  this.link = link;
+  this.deployedLink = deployedLink;
+  this.projectLink = projectLink;
   this.photo = photo;
   this.altTxt = altTxt;
 };
@@ -11,29 +12,34 @@ var Project = function (name, link, photo, altTxt) {
 const projects = [
   new Project(
     "Maze Runner",
+    "",
     "https://github.com/IsaacJCarnes/MazeGen",
     "./Assets/images/mazeGen.png",
     "The Maze Runner"
   ),
   new Project(
     "The Dog Endorsement Website",
+    "",
     "https://github.com/IsaacJCarnes/DogEndorsementWebsite",
     "./Assets/images/endorsementWebsite.png",
     "The Dog Endorsement Website"
   ),
   new Project(
     "Weather Dashboard",
+    "",
     "https://github.com/IsaacJCarnes/weather-dash-ij",
     "./Assets/images/weatherDash.png",
     "Weather Dashboard"
   ),
   new Project(
     "Work Calender",
+    "",
     "https://github.com/IsaacJCarnes/work-calender-ij",
     "./Assets/images/workDay.png",
     "Work Day Scheduler"
   ), new Project(
     "Scaredy Cat Scratch Game",
+    "",
     "https://scratch.mit.edu/projects/560457275/",
     "./Assets/images/scaredyCat.PNG",
     "Scaredy Cat on Scratch"
@@ -41,6 +47,23 @@ const projects = [
 ];
 
 var selectedProject = null;
+
+function showModal(projectNum){
+  selectedProject = projects[projectNum];
+
+  let modal_title = document.getElementById('modal-title');
+  modal_title.textContent = selectedProject.name;
+
+  let modal_img = document.getElementById('modal-img');
+  modal_img.setAttribute("src", selectedProject.photo);
+  modal_img.setAttribute("alt", selectedProject.alt);
+
+  let modal_deployed = document.getElementById('modal-deployed');
+  modal_deployed.setAttribute("href", selectedProject.deployedLink);
+
+  let modal_project = document.getElementById('modal-project');
+  modal_project.setAttribute("href", selectedProject.projectLink);
+};
 
 function fillShowcase(){
     projects.forEach(project => {
@@ -87,16 +110,13 @@ function selectProject(projectNum){
 }
 
 function showcaseSelect(event){
-    let elem = event.target;
-    if(elem.nodeName == "IMG"){
-        if(selectedProject == null){ //Adds reminder when first project is clicked
-            let reminderText = document.createElement("p");
-            reminderText.textContent = "Click Inside the Window to Redirect to the Project";
-            reminderText.setAttribute("style", "position: relative; left:"+(highlight.clientWidth/3)+"px; width: 150px; height: 10px; color:grey; font-size:5px;")
-            highlight.after(reminderText);
-        }
-        selectProject(elem.dataset.projectNum);
-    }
+  let elem = event.target;
+  if(elem.nodeName == "IMG"){
+    //selectProject(elem.dataset.projectNum);
+    showModal(elem.dataset.projectNum);
+  } else {
+    //document.getElementById('myModal').modal('hide');
+  }
 }
 
 function takeToProject(){
