bdf92504c6a58e6d665c29019301a68f9a1f3dcc
Isaac Carnes <isaacjcarnes@gmail.com>
2022-06-24 20:15:12 -0400

    Header now links to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ import Nav from "./Nav";
 
 export default function Header() {
     const navigate= useNavigate();
-    function handleClick() {
-        console.log("clicked");
+    function handleClick(e) {
+        e.preventDefault();
         navigate("/ij-portfolio/");
     }
 
     return(
         <div className="HeaderArea p-2 mt-2 rounded-top">
-            <h1 className="HeaderName" onClick={handleClick}>Isaac Joshua's Portfolio</h1>
+            <h1 className="HeaderName" onClick={(e) => handleClick(e)}>Isaac Joshua's Portfolio</h1>
 
             <Nav/>
         </div>
