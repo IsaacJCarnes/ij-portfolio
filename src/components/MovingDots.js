58200456d0881d5ff0fb0f58d1bbac08e1884a2c
Isaac Carnes <isaacjcarnes@gmail.com>
2022-09-04 21:33:39 -0400

    Added process for animating multiple objects

--- src/components/MovingDots.js
+++ src/components/MovingDots.js
@@ -9,28 +9,35 @@ export default function MovingDots(){
     console.log(window.innerHeight, window.innerWidth);
     const circleDiameter = 20; //pixels
     const circleVelocity = 80; //pixels
-    const [dotOptions, setDotOptions] = useState({objectOptions: {currentAngle: 30, leftPixel: 5, topPixel: 5}, styleOptions: {left: "5px", top:"5px", width: circleDiameter+"px", height: circleDiameter+"px"}});
+    const [dotOptions, setDotOptions] = useState([{objectOptions: {currentAngle: 30, leftPixel: 5, topPixel: 5}, styleOptions: {left: "5px", top:"5px", width: circleDiameter+"px", height: circleDiameter+"px"}}]);
 
-    const getCircleTransform = (isX) => {
+    const getCircleTransform = (isX, targetIndex) => { //Change second parametyer to be angle
         let piRatio = Math.PI/180;
         if(isX){
-            return Math.cos(dotOptions.objectOptions.currentAngle * piRatio);
+            return Math.cos(dotOptions[targetIndex].objectOptions.currentAngle * piRatio);
         }else{
-            return Math.sin(dotOptions.objectOptions.currentAngle * piRatio);
+            return Math.sin(dotOptions[targetIndex].objectOptions.currentAngle * piRatio);
         }
     }
 
     useEffect(() => {
-        let changeX =  dotOptions.objectOptions.leftPixel + (circleVelocity * getCircleTransform(true));
-        let changeY =  dotOptions.objectOptions.topPixel + (circleVelocity * getCircleTransform(false));
-        setDotOptions({objectOptions: {...dotOptions.objectOptions, leftPercent: changeX, topPercent: changeY}, styleOptions: {...dotOptions.styleOptions, left: changeX+"px", top:changeY+"px"}});
+        let newOptions = [...dotOptions];
+        newOptions.forEach((movingObj, index) => {
+            let changeX = movingObj.objectOptions.leftPixel + (circleVelocity * getCircleTransform(true, index));
+            let changeY =  movingObj.objectOptions.topPixel + (circleVelocity * getCircleTransform(false, index));
+            newOptions[index] = {objectOptions: {...movingObj.objectOptions, leftPixel: changeX, topPixel: changeY}, styleOptions: {...movingObj.styleOptions, left: changeX + 'px', top: changeY + 'px'}};
+        });
+        setDotOptions(newOptions);
     }, []);
 
-    const MoveCircle = () => { //Could use easing function for seconds when not going full width because of 
-        let tempAngle = dotOptions.objectOptions.currentAngle;
-        let changeX =  dotOptions.objectOptions.leftPixel + (circleVelocity * getCircleTransform(true));
-        let changeY =  dotOptions.objectOptions.topPixel + (circleVelocity * getCircleTransform(false));
-        console.log(getCircleTransform(false), getCircleTransform(true));
+    const MoveCircle = (targetIndex) => { //Could use easing function for seconds when not going full width because of 
+        let newOptions = [...dotOptions];
+        let tempObj = {...newOptions[targetIndex]};
+
+        let tempAngle = tempObj.objectOptions.currentAngle;
+        let changeX =  tempObj.objectOptions.leftPixel + (circleVelocity * getCircleTransform(true, targetIndex));
+        let changeY =  tempObj.objectOptions.topPixel + (circleVelocity * getCircleTransform(false, targetIndex));
+        console.log(getCircleTransform(false, targetIndex), getCircleTransform(true, targetIndex));
 
         if(changeX > pageWidth){
             changeX = pageWidth - circleDiameter;
@@ -47,10 +54,18 @@ export default function MovingDots(){
             changeY = 0;
             tempAngle = 360 - (tempAngle%360);
         }
-
-        setDotOptions({objectOptions: {...dotOptions.objectOptions, currentAngle: tempAngle, leftPixel: changeX, topPixel: changeY}, styleOptions: {...dotOptions.styleOptions, left: changeX+"px", top:changeY+"px"}});
+        newOptions[targetIndex] = {objectOptions: {...tempObj.objectOptions, currentAngle: tempAngle, leftPixel: changeX, topPixel: changeY}, styleOptions: {...tempObj.styleOptions, left: changeX + 'px', top: changeY + 'px'}};
+        setDotOptions(newOptions);
     }
+
+    const MovingObjects =
+                    dotOptions.map((element, index) => {
+                        return <div key={"movingObj"+index} className="movingDot" style={element.styleOptions} onTransitionEndCapture={(e) => {e.preventDefault(); MoveCircle(index)}}/>
+                    });
+
     return(
-        <div className="movingDot" style={dotOptions.styleOptions} onTransitionEndCapture={(e) => {e.preventDefault(); MoveCircle()}}/>
+        <div className='MovementPlatform'>
+            {MovingObjects}
+        </div>
     )
 }
