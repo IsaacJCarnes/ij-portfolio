1babf945343b70a145eec557d496214ec9c26907
IsaacJ <isaacjdeveloper@gmail.com>
2025-09-18 01:52:56 -0400

    Made background scroll horizontally on vertical scroll

--- src/App.js
+++ src/App.js
@@ -10,25 +10,27 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ContentModal from "./components/ContentModal";
 
+import { useState, useEffect } from "react";
+
 function App() {
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.scrollY/10);
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <div id="FullPage">
-      <div className="background"/>
+      <div className="background"  style={{ backgroundPositionX: offset+"px" }} />
       <div style={{ height: "50px" }}></div>
       <Header />
-      <ContentModal
-        isFlipped={true}
-        content={<AboutMe />}
-      />
-      <ContentModal content={<Portfolio />}/>
-      <ContentModal
-        isFlipped={true}
-        content={<Resume />}
-      />
-      <ContentModal
-        isFlipped={false}
-        content={<ContactMe />}
-      />
+      <ContentModal isFlipped={true} content={<AboutMe />} />
+      <ContentModal content={<Portfolio />} />
+      <ContentModal isFlipped={true} content={<Resume />} />
+      <ContentModal isFlipped={false} content={<ContactMe />} />
       <Footer />
     </div>
   );
