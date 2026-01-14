dd595f948eee10a5efa001e34e8a05bf7cf10191
IsaacJ <isaacjdeveloper@gmail.com>
2026-01-13 21:35:28 -0500

    Updated about me text

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,19 +1,11 @@
 import "./About.css";
 
 const AboutMe = () => {
+  const textContent = `Hi, I'm Isaac Carnes, a software engineer based in Pittsburgh, Pennsylvania, with over two and a half years of professional experience building software solutions. My passion for technology began early, from robotics camps in elementary school to competing on my middle school's Lego robotics team. While I initially set out to become a robotics engineer, I discovered my true calling in software development during high school when I began experimenting with code. Today, I bring that same curiosity and dedication to my work as a professional software engineer. Outside of my day-to-day responsibilities, I explore the intersection of technology and creativity through 3D modeling, sound design, and creative coding. I'm driven by a lifelong commitment to learning and pushing the boundaries of what's possible with technology.`;
   return (
     <div className="AboutContainer sectionContainer">
       <h1 className="sectionHeader">About Me</h1>
-      <p className="text-center">
-        Hi, I'm Isaac Carnes. I'm a software engineer from Pittsburgh,
-        Pennsylvania. My interests include but are not limited to, 3D modeling,
-        sound design and coding as it relates to art. My interest in technology
-        started at a young age. I spent several summers in elementary school in
-        robotics camps, and in middle school I joined my school's Lego
-        robotics team. Although originally I had planned to become a robotic
-        engineer, in 10th grade I started to learn and experiment with coding
-        and quickly realized that was my true passion in life.
-      </p>
+      <p className="text-center">{textContent}</p>
     </div>
   );
 };
